81983d0d8c337810f87b2fb4ecff21081fe5ac3f
Melad <meladhamedani@gmail.com>
2024-09-05 14:57:22 +0330

    Complete

--- Api/BackGroundService/KafkaBackgroundService.cs
+++ Api/BackGroundService/KafkaBackgroundService.cs
@@ -2,30 +2,32 @@ using Elastic.Apm.Api;
 
 namespace Api.BackGroundService;
 
-public class SendSMSScheduler : BackgroundService
+public class KafkaBackgroundService : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
             var transaction =
-                Elastic.Apm.Agent.Tracer.StartTransaction("Kafka", "Kafka");
+                Elastic.Apm.Agent.Tracer.StartTransaction("Kafka", ApiConstants.TypeMessaging);
             try
             {
-                await transaction.CaptureSpan("Publish to topic", ApiConstants.ActionExec, async (span) =>
+                await transaction.CaptureSpan("Publish to topic", "Kafka", async (span) =>
                 {
+                    //
+                    //Data Push to Topic 
                     span.Duration = 5000;
                     span.Outcome = Outcome.Success;
-                }, "Kafka", ApiConstants.SubTypeGrpc, true);
+                }, "Kafka", "Kafka", isExitSpan: true);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
             }
+
             transaction.End();
 
-            //SendSMS
             await Task.Delay(10000, stoppingToken);
         }
     }
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,7 +1,7 @@
 using System.Text.Json;
 using Api.BackGroundService;
 using Api.DataBase.Context;
-using Api.Entities;
+using Api.Entities; 
 using Elastic.Apm.Api;
 using Elastic.Apm.NetCoreAll;
 using Elastic.Apm.StackExchange.Redis;
@@ -11,12 +11,16 @@ using StackExchange.Redis;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<SensorDbContext>(q => q.UseSqlite(builder.Configuration.GetConnectionString("SqlLite")!));
-builder.Services.AddHostedService<SendSMSScheduler>();
+
+builder.Services.AddHostedService<KafkaBackgroundService>();
+
 var multiplexer =
     ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis")!);
 multiplexer.UseElasticApm();
 builder.Services.AddSingleton<IConnectionMultiplexer>(multiplexer);
+
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
@@ -35,16 +39,26 @@ app.MapGet("/SetSensorData", async (SensorDbContext dbContext, IConnectionMultip
         var response = await client.GetAsync(url);
         response.EnsureSuccessStatusCode();
         var jsonResponse = await response.Content.ReadAsStringAsync();
-        
-     
-        var sensorData = SensorDataGenerator.GenerateRandomSensorData();
+        var transaction = Elastic.Apm.Agent.Tracer.CurrentTransaction;
+
+        var sensorData = new SensorData();
+        await transaction.CaptureSpan("GenerateRandomSensorData", "GenerateRandomSensorData", async (span) =>
+        {
+            sensorData = SensorDataGenerator.GenerateRandomSensorData();
+            span.Duration = 2000;
+            span.Outcome = Outcome.Failure;
+        });
+
+
         await db.StringSetAsync("sensor_data", JsonSerializer.Serialize(sensorData));
+        
         await dbContext.SensorData.AddAsync(sensorData);
         await dbContext.SaveChangesAsync();
+
         
-        var transaction = Elastic.Apm.Agent.Tracer.CurrentTransaction;
         await transaction.CaptureSpan("محاسبه وضعیت IOT", "محاسبه وضعیت IOT", async (span) =>
         {
+            
             await Task.Delay(1000);
             span.Outcome = Outcome.Success;
         });
